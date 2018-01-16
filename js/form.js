0b1d8aa5d0fa13b847a58720ae3db658b8823794
41521339880 <41521339880@info.local>
2018-01-16 21:55:42 -0200

    Criação de função de validação

--- js/form.js
+++ js/form.js
@@ -1,5 +1,6 @@
 var clientInfo={
     address:{
+        country: 'Afhganistan'
 
     },
     contact:{
@@ -10,6 +11,20 @@ var clientInfo={
 
 
 function formValidation(){
+    
+event.preventDefault();
+
+    _addressObj = {
+        country: document.getElementById("selection_country").value,
+        zip: document.getElementById("zip").value,
+        state: document.getElementById("state").value,
+        city: document.getElementById("selection_city").value,
+        ad1: document.getElementById("ad1").value,
+        ad2: document.getElementById("ad2").value
+    };
+
+    addressValidation(_addressObj)
+    console.log(_addressObj);
 
 }
 
